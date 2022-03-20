51014e67ecb873dd7e5e35b9df155f64a66b4918
Ajay o s <akhubsmaster@gmail.com>
2022-03-20 22:15:57 +0530

    Update bill.c

--- bill.c
+++ bill.c
@@ -3,8 +3,8 @@
 void main()
 {  
     char cu_name[25]; // cu_name = Customer Name 
-    int info;
-    float unit, charge, ca_charge=0;
+    int info, surchg;
+    float unit, charge, ca_charge=0, tt_amout;
    printf("Input the name of the customer :");
    scanf("%s",cu_name);
    printf("Input the unit consumed by the customer : ");
@@ -14,30 +14,30 @@ void main()
         { 
             charge =3.15;
             ca_charge+=unit * charge;
-        } else if (unit <=100) 
+        } else if (unit <=101) 
         {
-            info = unit - 50;
+            info = unit - 51;
             ca_charge+= 3.15 * 50;
             charge =3.7;
             ca_charge+= info * charge;
-        } else if (unit <=150) 
+        } else if (unit <=151) 
         {
-            info = unit - 100;
+            info = unit - 101;
             ca_charge+= 3.15 * 50;
             ca_charge+= 3.7 * 50;
             charge =4.8;
             ca_charge+= info * charge;
-        } else if (unit <=200) 
+        } else if (unit <=201) 
         {
-            info = unit - 150;
+            info = unit - 151;
             ca_charge+= 3.15 * 50;
             ca_charge+= 3.7 * 50;
             ca_charge+= 4.8 * 50;
             charge =6.4;
             ca_charge+= info * charge;
-        } else if (unit <=250) 
+        } else if (unit <=251) 
         {
-            info = unit - 200;
+            info = unit - 201;
             ca_charge+= 3.15 * 50;
             ca_charge+= 3.7 * 50;
             ca_charge+= 4.8 * 50;
@@ -45,10 +45,13 @@ void main()
             charge =7.6;
             ca_charge+= info * charge;
         }
-
+    surchg = 80;
+    tt_amout = ca_charge + surchg;
    printf("\nElectricity Bill\n");
    printf("Customer Name                       :%s\n",cu_name);
    printf("unit used                           :%f\n",unit);
-   printf("Charges                             :%f\n",ca_charge);
+   printf("Energr charge                       :%f\n",ca_charge);
+   printf("Surchage Amount                     :%d\n",surchg);
+   printf("Tottal Amount                       :%f\n",tt_amout);
 
 } 
